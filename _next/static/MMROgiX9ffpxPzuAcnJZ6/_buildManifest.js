d15ec5550950d3cb9f24517556415988d098e205
Mastermind <maksimlunin@MacBook-Pro.local>
2026-07-07 13:46:23 +0300

    Deploy GitHub Pages

---
+++ _next/static/MMROgiX9ffpxPzuAcnJZ6/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/signature-import-site//_next/:path+",
+        "destination": "/signature-import-site/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
